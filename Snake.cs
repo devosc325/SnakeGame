ebe580cc3cf86053c22132872a094a9040d77658
devosc325 <128750248+devosc325@users.noreply.github.com>
2024-05-09 16:22:49 -0400

    Create Snake.cs
    this is C# code for the snake and movement in the game

---
+++ Snake.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments = new List<Transform>();
+    public Transform segmentPrefab;
+    public int initialSize = 4;
+    public logicscript Logic;
+    public bool snakeisAlive = true;
+
+
+
+
+    private void Start()
+    {
+        ResetState();
+
+        Logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<logicscript>();
+
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && snakeisAlive) {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && snakeisAlive) {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) && snakeisAlive) {
+            _direction = Vector2.left;
+        } else if (Input. GetKeyDown(KeyCode.RightArrow) && snakeisAlive) {
+            _direction = Vector2.right;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = _segments.Count -1; i > 0; i--) {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+
+    }
+
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+
+    }
+
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++) {
+            Destroy(_segments[i].gameObject);
+
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i = 1; i < this.initialSize; i++) {
+            _segments.Add(Instantiate(this.segmentPrefab));
+
+        }
+
+
+        this.transform.position = Vector3.zero;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food") {
+            Grow();
+        } else if (other.tag == "Obstacle") {
+            Logic.gameOver();
+
+            snakeisAlive = false;
+
+    }
+
+   
+}
+}
